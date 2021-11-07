30b568aa7b64a3d79ef5aeeeb70b6716d53fbe47
Juan Cime <92643359+Kichye@users.noreply.github.com>
2021-11-07 15:47:05 -0600

    Add files via upload

---
+++ P_2E_2/P_2E_2/Controllers/AlcController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using P_2E_2.Domain;
+using P_2E_2.Services;
+
+namespace P_2E_2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AlcController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult CalcularVolumen(Alc al)
+        {
+            var alser = new AlServices();
+
+            var tac = alser.CalcAlc(al.bebida, al.cantidad);
+
+            if (tac == 0)
+            {
+                return Ok("Bebida no encontrada.");
+            }
+
+            var CantAlS = alser.CalcAlcSan(tac);
+
+            var masa = alser.CalM(CantAlS);
+
+            var vs = alser.CalVs(al.peso);
+
+            var volal = alser.CVolAlcohol(masa, vs);
+
+            var res = alser.Validar(volal);
+
+            return Ok(res);
+        }
+    }
+}
---
+++ P_2E_2/P_2E_2/Domain/Alc.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace P_2E_2.Domain
+{
+    public record Alc (string bebida, int cantidad, double peso, string genero);
+}
